9a54ea94090c80b2d6458584cb2285d790c33a78
tanakatarou321 <tanatana.tarotaro321@gmail.com>
2024-03-24 16:52:36 +0900

    initial release

---
+++ include/print_hierarchy/print_hierarchy.h
@@ -0,0 +1,20 @@
+#include <systemc.h>
+#ifdef _PRINT_HIERARCHY_
+#define _PRINT_HIERARCHY_
+void _print_hierarchy(sc_object* parent){
+    std::cout << parent->name() << "," << parent->kind() << std::endl;
+    if (parent->kind() == "sc_module"){
+        for (int i = 0; i < parent->get_child_objects().size(); i++) {
+            sc_object* children = parent->get_child_objects()[i];
+            _print_hierarchy(children);
+        }
+    }
+}
+
+void print_hierarchy(){
+    for (int i = 0; i < sc_get_top_level_objects().size(); i++) {
+        sc_object* children = sc_get_top_level_objects()[i];
+        _print_hierarchy(children);
+    }
+}
+#endif
---
+++ include/sc-utils.h
@@ -0,0 +1,8 @@
+#include <systemc.h>
+
+#ifdef _SC_UTIL_
+#define _SC_UTIL_
+
+#include "print_hierarchy/print_hierarchy.h"
+
+#endif
